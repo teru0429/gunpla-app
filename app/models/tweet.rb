5c31fd02338b1a696768c804e723b2f984038653
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-01-22 14:04:16 +0900

    複数投稿機能

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_one_attached :image
+  has_many_attached :images
 
   validates :title, presence: true
   validates :text, presence: true
