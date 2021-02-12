bb112c3e3717260bbbd6fa372cd27b1b3ff8889c
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-02-12 23:16:01 +0900

    チャットテストコード

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   # ↓↓アソシエーション↓↓
   has_many :room_users, dependent: :destroy
-  has_many :users, through: :room_users
+  has_many :users,      through: :room_users
   has_many :chats,      dependent: :destroy
 end
--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:chat)
+  end
+
+  describe 'メッセージ入力時' do
+
+    context 'メッセージが入力できている時' do
+      it 'messageが入力されていて保存できること' do
+        expect(@message).to be_valid
+      end
+    end
+
+    context 'メッセージが入力できていない時' do
+      it 'messageが空では保存できないこと' do
+        @message.message = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Message can't be blank")
+      end
+
+      it 'roomが紐付いていないと保存できないこと' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Room must exist")
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
